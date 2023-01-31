6977ac73aaa4558e304459219fd5442b91798a9f
SERVER <SERVER@SERVER-PC>
2023-01-31 14:40:08 +0530

    [vaishnavi]:demo2 updated

---
+++ src/gitHub/demo1.java
@@ -0,0 +1,8 @@
+package gitHub;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+}
---
+++ src/gitHub/demo2.java
@@ -0,0 +1,8 @@
+package gitHub;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
